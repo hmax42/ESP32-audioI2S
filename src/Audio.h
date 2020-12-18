131284094639ef6e722c568b0a742c293be50d2f
Theexternaldisk <Theexternaldisk@users.noreply.github.com>
2020-12-18 14:10:19 +0100

    added loop support for files
    with these changes it is possible to loop a file without interrupting the playback.
    What it does is safe where the audio data starts in the file and when the eof is detected jump back to the start.
    will need further testing

--- src/Audio.h
+++ src/Audio.h
@@ -107,6 +107,7 @@ public:
     ~Audio();
     bool connecttoFS(fs::FS &fs, String file);
     bool connecttoSD(String sdfile);
+    bool setFileLoop(bool input);//TEST loop
     bool connecttohost(String host, const char* user = "", const char* pwd = "");
     bool connecttospeech(String speech, String lang);
     void loop();
@@ -261,6 +262,11 @@ private:
     uint32_t        m_audioFileDuration = 0;
     float           m_audioCurrentTime = 0;
     uint8_t          power = 0;
+    //TEST loop
+    bool            m_f_loop = false;               // Set if audio file should loop
+    size_t          m_f_loop_point = 0;             // Point in the file where the audio data starts
+    size_t          m_f_file_size = 0;              // size of the file 
+    //TEST loop 
 };
 
 #endif /* AUDIO_H_ */
